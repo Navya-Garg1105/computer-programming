786f25daf0d3d6e7dd66ea7ad1f3ce6b5558241c
Navya-Garg1105 <navya.motikunj@gmail.com>
2024-11-25 14:52:53 +0530

    added coading question for string

---
+++ coding-assignment/string/string_q3.py
@@ -0,0 +1,6 @@
+'''create  a program that replaces all occurances of a specific word in a string with another word'''
+str = input()
+word = input()
+substitute = input()
+v =  str.replace(word,substitute)
+print(v)
