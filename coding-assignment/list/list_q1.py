420fce2f8d3b16716316945dee5d23fc1c3324f9
Navya-Garg1105 <navya.motikunj@gmail.com>
2024-11-27 14:28:26 +0530

    accessing elements of a list

---
+++ coding-assignment/list/list_q1.py
@@ -0,0 +1,5 @@
+#  Access Elements
+# Access a specific element from the list.
+my_list = input("Enter elements of the list separated by spaces: ").split()
+index = int(input("Enter the index to access: "))
+print("Element at index", index, ":", my_list[index])
