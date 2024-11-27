eb47d1c5a7d75bb2a98746b9e77845ba8549ef0a
Navya-Garg1105 <navya.motikunj@gmail.com>
2024-11-27 14:31:55 +0530

    addition of list

---
+++ coding-assignment/list/list_q5.py
@@ -0,0 +1,5 @@
+# Concatenate Two Lists
+list1 = input("Enter elements of the first list separated by spaces: ").split()
+list2 = input("Enter elements of the second list separated by spaces: ").split()
+concatenated_list = list1 + list2
+print("Concatenated list:", concatenated_list)
