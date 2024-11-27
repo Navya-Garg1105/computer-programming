790a6b7a6f668f5867f52ef7408766d4159c964c
Navya-Garg1105 <navya.motikunj@gmail.com>
2024-11-27 18:07:01 +0530

    code for function

---
+++ coding-assignment/function/func_q3.py
@@ -0,0 +1,10 @@
+# Find the GCD of Two Numbers
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print(f"The GCD of {num1} and {num2} is {gcd(num1, num2)}.")
+
