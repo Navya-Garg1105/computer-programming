157ff2342881242b30f4560059289c03bff93deb
Navya-Garg1105 <navya.motikunj@gmail.com>
2024-11-27 18:08:45 +0530

    palindrome string

---
+++ coding-assignment/function/func_q5.py
@@ -0,0 +1,10 @@
+# Check Palindrome String
+def is_palindrome(s):
+    return s == s[::-1]
+
+string = input("Enter a string to check if it's a palindrome: ")
+if is_palindrome(string):
+    print(f'"{string}" is a palindrome.')
+else:
+    print(f'"{string}" is not a palindrome.')
+
